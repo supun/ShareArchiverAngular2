fa9e2ec0c9b8dcdf1cba07e434f64c2f287b5642
Supun dharmarathna <supun921@gmail.com>
2017-03-28 13:35:43 +0530

    material UI integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
+import {MaterialModule} from '@angular/material';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    [MaterialModule.forRoot(),FlexLayoutModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
